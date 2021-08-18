700ef0fc7d506e96be4788398d05d5cc8b450375
Grace Sutherland <rgs1690@gmail.com>
2021-08-18 09:55:51 -0500

    fixed all errors

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,23 +3,115 @@
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
+const studentArray = [];
+const voldermortsArmy = [];
+
+const renderToDOM = (divId, content) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = content;
+};
+
+const showForm = () => {
+  document.querySelector('#button-div').innerHTML = '';
+  const content = `<div class="card m-3 justify-content-center">
+                        <div class="card-body text-center">  
+                            <h4>
+                                Enter First Year's Name
+                            </h4>
+                            <div id="error-message"></div>
+                            <div class="d-flex col-md-6 offset-md-3">
+                                <label for="inlineFormInput">Student: </label>
+                                <input type="text" class="form-control mx-3" id="student-name" placeholder="Luna Lovegood" required>
+                               <button id="sort" class="btn btn-primary mb-2">Sort!</button>
+                            </div>
+                        </div>
+                    </div>`;
+  renderToDOM('#sorting-form', content);
+};
+
+const sortingHat = () => {
+  const houses = ['gryffindor', 'slytherin', 'hufflepuff', 'ravenclaw'];
+  const sortHat = houses[Math.floor(Math.random() * houses.length)];
+  return sortHat;
+};
+
+const sortStudentsByHouse = (array) => array.sort((a, b) => (a.house > b.house ? 1 : -1));
+
+const cardCreator = (divId, array) => {
+  let card = '';
+  for (let i = 0; i < array.length; i++) {
+    if (divId.includes('voldermort')) {
+      card += `<div class="card m-3" style="width: 18rem;">
+                    <img class="card-img-top" src="https://vignette.wikia.nocookie.net/harrypotter/images/d/d4/Death_Eaters_WBST.png/revision/latest?cb=20161205041948" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text">Sadly, <b>${array[i].name}</b> went over to the dark side!</p>
+                    </div>
+                </div>`;
+    } else {
+      card += `<div class="card m-3" style="min-width: 300px;" id="${i}">
+                    <div class="row no-gutters">
+                        <div class="col-md-4" style="min-height: 150px; background-color: $houseColors[array[i].house]
+                        }">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">${array[i].name}</h5>
+                                <p class="card-text">${array[i].house.toUpperCase()}</p>
+                                <button type="button" id="${i}" class="btn btn-danger">EXPEL</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+    }
+  }
+
+  renderToDOM(divId, card);
+};
+
+const errorMessage = (student) => {
+  if (student) {
+    document.querySelector('#error-message').innerHTML = '';
+  } else {
+    document.querySelector('#error-message').innerHTML = `
+      <div style="color: red;margin-bottom: 10px;">
+        <b>Please type a name</b>
+      </div>`;
+  }
+};
+const addStudentToArray = (e) => {
+  if (e.target.id === 'sort' || e.keyCode === 13) {
+    const student = document.querySelector('#student-name');
+    errorMessage(student.value);
+
+    if (student.value) {
+      studentArray.push({
+        name: student.value,
+        house: sortingHat(),
+      });
+      student.value = '';
+    }
+
+    const sortedArray = sortStudentsByHouse(studentArray);
+    cardCreator('#first-years-card-display', sortedArray);
+  }
+};
+
+const expelStudent = (e) => {
+  if (e.target.type === 'button') {
+    voldermortsArmy.push(studentArray[e.target.id]);
+    studentArray.splice(e.target.id, 1);
+    cardCreator('#first-years-card-display', studentArray);
+    cardCreator('#voldermorts-army', voldermortsArmy);
+  }
+};
+const domEvents = () => {
+  document.querySelector('#sorting-form').addEventListener('click', addStudentToArray);
+  document.querySelector('#first-years-card-display').addEventListener('click', expelStudent);
+  document.querySelector('#start-sorting').addEventListener('click', showForm);
+  document.querySelector('#student-name').addEventListener('keyup', addStudentToArray); // this could have easily been accomplished by using a form tag instead of using separate form elements as form submits on enter key press also
+};
 const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
-  `;
-  console.warn('YOU ARE UP AND RUNNING!');
-
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
-
-  // USE WITH FIREBASE AUTH
-  // checkLoginStatus();
+  domEvents();
 };
 
 init();
